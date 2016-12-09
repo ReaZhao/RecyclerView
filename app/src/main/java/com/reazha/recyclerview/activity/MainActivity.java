985d7dc20ba12ef5a039b25ac9f53e35bd3d193c
ReaZhao <377742053@qq.com>
2016-12-09 18:45:37 +0800

    RecyclerView 点击事件，长按事件，添加，删除

--- app/src/main/java/com/reazha/recyclerview/activity/MainActivity.java
+++ app/src/main/java/com/reazha/recyclerview/activity/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends Activity {
     private RecyclerView idRecyclerview;
     private ArrayList<String> mDatas;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +41,11 @@ public class MainActivity extends Activity {
         idRecyclerview = (RecyclerView) findViewById(R.id.id_recyclerview);
         //设置布局管理器
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        //水平布局  和垂直布局
+        //水平布局  和垂直布局 默认是LinearLayout.VERTICAL
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         idRecyclerview.setLayoutManager(linearLayoutManager);
         //设置适配器
-        idRecyclerview.setAdapter(new MainAdapter(this,mDatas));
+        idRecyclerview.setAdapter(new MainAdapter(this, mDatas));
     }
+
 }
--- app/src/main/java/com/reazha/recyclerview/adapter/ViewHolderRecycler.java
+++ app/src/main/java/com/reazha/recyclerview/adapter/ViewHolderRecycler.java
@@ -13,7 +13,7 @@ import android.view.View;
  */
 
 public class ViewHolderRecycler extends RecyclerView.ViewHolder {
-    private  SparseArray<View> views = new SparseArray<View>();
+    public SparseArray<View> views = new SparseArray<View>();
     private View view;
 
     public ViewHolderRecycler(View itemView) {
@@ -23,12 +23,19 @@ public class ViewHolderRecycler extends RecyclerView.ViewHolder {
     }
 
     public <T extends View> T getView(int resId) {
-        View v = views.get(resId);
-        if (null == v) {
-            v = view.findViewById(resId);
-            views.put(resId, v);
+        View v=views.get(resId);
+        if(v==null){
+            v=view.findViewById(resId);
+            views.put(resId,v);
         }
         return (T) v;
     }
-
+//    public interface OnItemClickLinstener{
+//        void OnItemClick(ViewHolderRecycler view, int position);
+//        void OnItemLongClick(ViewHolderRecycler view, int position);
+//    }
+//    public OnItemClickLinstener mOnItemClickLinstener;
+//    public void setOnItemClickLinstener(OnItemClickLinstener linstener){
+//        this.mOnItemClickLinstener=linstener;
+//    }
 }
